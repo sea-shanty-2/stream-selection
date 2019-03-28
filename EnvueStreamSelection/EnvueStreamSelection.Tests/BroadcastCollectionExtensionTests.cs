3e1166254e470d0338986061f18a58eede794b21
Anders Jakobsen <andersjkbsn@gmail.com>
2019-03-28 10:30:33 +0100

    More tests

--- EnvueStreamSelection/EnvueStreamSelection.Tests/BroadcastCollectionExtensionTests.cs
+++ EnvueStreamSelection/EnvueStreamSelection.Tests/BroadcastCollectionExtensionTests.cs
@@ -21,26 +21,39 @@ namespace EnvueStreamSelection.Tests
             var broadcast = new MockBroadcast {Bitrate = 0};
             var normalised = new List<IBroadcast> {broadcast}.GetNormalisedBitrates();
 
-            Assert.AreEqual(normalised[broadcast], 0);
+            Assert.AreEqual(0D, normalised[broadcast]);
         }
 
         [Test]
-        public void TestNormaliseSingleValidBitrate([Values(10, 100, 1000, 10000)] int bitrate)
+        public void TestNormaliseSingleValidBitrate([Values(512, 1024, 2048, 4096)] int bitrate)
         {
             var broadcast = new MockBroadcast {Bitrate = bitrate};
             var normalised = new List<IBroadcast> {broadcast}.GetNormalisedBitrates();
 
-            Assert.AreEqual(normalised[broadcast], 1D);
+            Assert.AreEqual(1D, normalised[broadcast]);
         }
 
         [Test]
         public void TestNormaliseMultipleValidBitrates()
         {
-            var lowBitrateBroadcast = new MockBroadcast { Bitrate = 500 * 1024 };
-            var highBitrateBroadcast = new MockBroadcast { Bitrate = 2000 * 1024 };
-            var normalised = new List<IBroadcast> { lowBitrateBroadcast, highBitrateBroadcast }.GetNormalisedBitrates();
+            var lowBitrateBroadcast = new MockBroadcast { Bitrate = 512 * 1000 };
+            var mediumBitrateBroadcast = new MockBroadcast { Bitrate = 2048 * 1000 };
+            var highBitrateBroadcast = new MockBroadcast { Bitrate = 4096 * 1000 };
+            var normalised = new List<IBroadcast> { lowBitrateBroadcast, mediumBitrateBroadcast, highBitrateBroadcast }.GetNormalisedBitrates();
             
-            Assert.Less(normalised[lowBitrateBroadcast], normalised[highBitrateBroadcast]);
+            Assert.Less(normalised[lowBitrateBroadcast], normalised[mediumBitrateBroadcast]);
+            Assert.Less(normalised[mediumBitrateBroadcast], normalised[highBitrateBroadcast]);
+        }
+
+        [Test]
+        public void TestNormaliseMultipleInvalidBitrateMixin()
+        {
+            var invalidBitrateBroadcast = new MockBroadcast {Bitrate = 0};
+            var validBitrateBroadcast = new MockBroadcast { Bitrate = 2048 * 1000 };
+            var normalised = new List<IBroadcast> { invalidBitrateBroadcast, validBitrateBroadcast }.GetNormalisedBitrates();
+
+            Assert.AreEqual(0D, normalised[invalidBitrateBroadcast]);
+            Assert.AreEqual(1D, normalised[validBitrateBroadcast]);
         }
     }
 }
--- EnvueStreamSelection/EnvueStreamSelection/Broadcast/BroadcastCollectionExtensions.cs
+++ EnvueStreamSelection/EnvueStreamSelection/Broadcast/BroadcastCollectionExtensions.cs
@@ -5,13 +5,13 @@ namespace EnvueStreamSelection
 {
     public static class BroadcastCollectionExtensions
     {
-        public static Dictionary<IBroadcast, int> GetNormalisedBitrates(this ICollection<IBroadcast> broadcasts)
+        public static Dictionary<IBroadcast, double> GetNormalisedBitrates(this ICollection<IBroadcast> broadcasts)
         {
             if (broadcasts.Count == 0)
-                return new Dictionary<IBroadcast, int>();
+                return new Dictionary<IBroadcast, double>();
             
             var maxBitrate = broadcasts.Max(e => e.Bitrate);
-            return broadcasts.ToDictionary(b => b, b => maxBitrate > 0 ? b.Bitrate / maxBitrate : 0);
+            return broadcasts.ToDictionary(b => b, b => maxBitrate > 0 ? b.Bitrate / (double) maxBitrate : 0D);
         }
     }
 }
