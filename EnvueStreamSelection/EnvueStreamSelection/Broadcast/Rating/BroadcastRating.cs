c1cd81eef8d0ba34db67381cbe21ba816384e272
Anders Jakobsen <andersjkbsn@gmail.com>
2019-05-15 11:22:53 +0200

    Removed setters from some properties

--- EnvueStreamSelection/EnvueStreamSelection/Broadcast/Rating/BroadcastRating.cs
+++ EnvueStreamSelection/EnvueStreamSelection/Broadcast/Rating/BroadcastRating.cs
@@ -2,8 +2,8 @@ namespace EnvueStreamSelection.Broadcast.Rating
 {
     public class BroadcastRating : IBroadcastRating
     {
-        public int Weight { get; set; }
-        public RatingPolarity Polarity { get; set; }
+        public int Weight { get; }
+        public RatingPolarity Polarity { get; }
 
         public BroadcastRating(RatingPolarity polarity, int weight = 1)
         {
